746a0bbf1bfc5f58ea4c685cba0b26221d51fea5
YakerHuertas26 <123754558+YakerHuertas26@users.noreply.github.com>
2024-10-01 22:30:25 -0500

    teoria final

--- src/rute/Home.jsx
+++ src/rute/Home.jsx
@@ -5,7 +5,9 @@ const Home = () => {
     const {listaPokemon,mostrarPokemon}= useStorePokemon()
     const {isPending, isError, data, error}= useQuery({
         queryKey:["mostrar Pokemon"],
-        queryFn:mostrarPokemon
+        queryFn:mostrarPokemon,
+        // para no estar haciendo consultas a la Api a cada rato
+        staleTime:Infinity
     })
     if (isPending) {
         return <span>Loading...</span>
